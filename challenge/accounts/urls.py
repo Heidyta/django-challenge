95b78b789e63089a60fb7792e7e2c53c06556daa
Heidy Abreu fumero <heidyabreufumero@MacBook-Air-de-Heidy.local>
2020-02-19 20:12:26 -0500

    FizzBuzz view url configuration

---
+++ challenge/accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+from . import views
+from rest_framework import routers
+
+app_name = 'api'
+
+router = routers.SimpleRouter()
+
+router.register(r'accounts', views.AccountViewSet)
+
+urlpatterns = [
+                path('', include(router.urls)),
+                path(r'fizz-buzz/', views.FuzzyView().fuzzy)
+               ]
